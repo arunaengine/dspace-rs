4723748756c1e86414f24de22df54227beb538ac
Maria Hansen <maria.hansen@computational.bio.uni-giessen.de>
2024-11-26 15:18:44 +0100

    test: add tests of the corresponding api calls of the contract negotiation provider endpoints of the aruna connector prototype

--- tests/aruna_connector/negotiation_provider_api_test.rs
+++ tests/aruna_connector/negotiation_provider_api_test.rs
@@ -1,16 +1,16 @@
 mod common;
 
 mod aruna_connector_negotiation_provider_api_test {
-    use uuid::Uuid;
-    use dsp_api::contract_negotiation::{AbstractPolicyRule, Action, ContractNegotiationEventMessage, ContractRequestMessage, MessageOffer, Permission, PolicyClass, Target};
-    use dsp_api::contract_negotiation::contract_negotiation_event_message::EventType;
+    use tokio::time::sleep;
+    use crate::common::{setup_dsp_provider_configuration, setup_management_consumer, setup_provider_configuration, DATASPACE_PROTOCOL, PROVIDER_ID};
     use dsp_client::contract_negotiation::negotiation_provider_api;
-
-    use crate::common::{setup_dsp_provider_configuration, PROVIDER_ID};
+    use edc_api::{AssetInput, ContractOfferDescription, Offer};
+    use edc_client::{asset_api, contract_negotiation_api};
+    use odrl::name_spaces::LD_NS;
+    use uuid::Uuid;
 
     #[tokio::test]
     async fn test_get_unknown_negotiation() {
-
         let config = setup_dsp_provider_configuration().await;
         let pid = "Test";
 
@@ -20,98 +20,177 @@ mod aruna_connector_negotiation_provider_api_test {
 
     #[tokio::test]
     async fn test_get_negotiation() {
-
+        unimplemented!()
     }
 
     #[tokio::test]
     async fn request_negotiation() {
 
-        let request_message = ContractRequestMessage {
-            context: std::collections::HashMap::from([("dspace".to_string(), serde_json::Value::String("https://w3id.org/dspace/v0.8/".to_string()))]),
-            dsp_type: "dspace:ContractRequestMessage".to_string(),
-            provider_pid: None,
-            consumer_pid: Uuid::new_v4().to_string(),
-            offer: MessageOffer {
-                policy_class: PolicyClass {
-                    abstract_policy_rule: AbstractPolicyRule { assigner: Some(PROVIDER_ID.to_string()), assignee: None },
-                    id: "free-use-policy".to_string(),
-                    profile: vec![],
-                    permission: vec![Permission {
-                        abstract_policy_rule: AbstractPolicyRule { assigner: Some("aruna-connector".to_string()), assignee: None },
-                        action: Action::Use,
-                        constraint: vec![],
-                        duty: None,
-                    }],
-                    obligation: vec![],
-                    target: Target::new("FreeUseResource".to_string()),
-                },
-                odrl_type: "odrl:Offer".to_string(),
-            },
-            callback_address: "http://consumer-connector:9194/protocol".to_string(),
+        let consumer = setup_management_consumer().await;
+        let provider = setup_provider_configuration();
+
+        let asset_id = Uuid::new_v4().to_string();
+
+        let asset_input = AssetInput {
+            context: std::collections::HashMap::from([("@vocab".to_string(), serde_json::Value::String("https://w3id.org/edc/v0.0.1/ns/".to_string()))]),
+            at_id: Some(asset_id.clone()),
+            at_type: Some("Asset".to_string()),
+            data_address: Box::new(Default::default()),
+            private_properties: None,
+            properties: std::collections::HashMap::from([
+                ("name".to_string(), serde_json::Value::String("test".to_string())),
+                ("foo".to_string(), serde_json::Value::String("bar".to_string()))
+            ]),
         };
 
-        let config = setup_dsp_provider_configuration().await;
+        let asset_response = asset_api::create_asset(&provider, Some(asset_input)).await;
 
-        let result = negotiation_provider_api::request_negotiation(&config, request_message).await;
+        assert!(asset_response.is_ok());
 
-        println!("{:?}", result);
+        let offer_id = Uuid::new_v4().to_string();
+
+        let offer = Offer {
+            context: Some(LD_NS.to_string()),
+            at_type: Some("Offer".to_string()),
+            at_id: offer_id.clone(),
+            assigner: PROVIDER_ID.to_string(),
+            target: asset_id.clone(),
+        };
+
+        let policy: serde_json::Value = serde_json::json!({
+            "@context": offer.context,
+            "@type": offer.at_type,
+            "@id": offer.at_id,
+            "assigner": offer.assigner,
+            "target": offer.target,
+        });
+
+        let offer_description = ContractOfferDescription {
+            at_type: Some("OfferDescription".to_string()),
+            asset_id: Some(asset_id.clone()),
+            offer_id: Some(offer_id),
+            policy: Some(policy),
+        };
+
+        let contract_request = edc_api::ContractRequest {
+            context: std::collections::HashMap::from([("@vocab".to_string(), serde_json::Value::String("https://w3id.org/edc/v0.0.1/ns/".to_string()))]),
+            at_type: Some("ContractRequest".to_string()),
+            callback_addresses: None,
+            connector_address: None,
+            counter_party_address: "http://localhost:3000".to_string(),
+            offer: Some(offer_description),
+            policy: Some(offer.clone()),
+            protocol: DATASPACE_PROTOCOL.to_string(),
+            provider_id: None,
+        };
+
+        let contract_negotiation_response = contract_negotiation_api::initiate_contract_negotiation(&consumer, Some(contract_request)).await;
+
+        match contract_negotiation_response {
+            Ok(response) => {
+                assert!(response.at_id.is_some());
+                assert!(response.created_at.is_some());
+            }
+            Err(e) => {
+                panic!("Error: {:#?}", e);
+            }
+        }
     }
 
     #[tokio::test]
     async fn accept_negotiation() {
+        unimplemented!()
+    }
+    
+    #[tokio::test]
+    async fn terminate_negotiation() {
+        let consumer = setup_management_consumer().await;
+        let provider = setup_provider_configuration();
 
-        let request_message = ContractRequestMessage {
-            context: std::collections::HashMap::from([("dspace".to_string(), serde_json::Value::String("https://w3id.org/dspace/v0.8/".to_string()))]),
-            dsp_type: "dspace:ContractRequestMessage".to_string(),
-            provider_pid: None,
-            consumer_pid: Uuid::new_v4().to_string(),
-            offer: MessageOffer {
-                policy_class: PolicyClass {
-                    abstract_policy_rule: AbstractPolicyRule { assigner: Some(PROVIDER_ID.to_string()), assignee: None },
-                    id: "free-use-policy".to_string(),
-                    profile: vec![],
-                    permission: vec![Permission {
-                        abstract_policy_rule: AbstractPolicyRule { assigner: Some("aruna-connector".to_string()), assignee: None },
-                        action: Action::Use,
-                        constraint: vec![],
-                        duty: None,
-                    }],
-                    obligation: vec![],
-                    target: Target::new("FreeUseResource".to_string()),
-                },
-                odrl_type: "odrl:Offer".to_string(),
-            },
-            callback_address: "http://consumer-connector:9194/protocol".to_string(),
+        let asset_id = Uuid::new_v4().to_string();
+
+        let asset_input = AssetInput {
+            context: std::collections::HashMap::from([("@vocab".to_string(), serde_json::Value::String("https://w3id.org/edc/v0.0.1/ns/".to_string()))]),
+            at_id: Some(asset_id.clone()),
+            at_type: Some("Asset".to_string()),
+            data_address: Box::new(Default::default()),
+            private_properties: None,
+            properties: std::collections::HashMap::from([
+                ("name".to_string(), serde_json::Value::String("test".to_string())),
+                ("foo".to_string(), serde_json::Value::String("bar".to_string()))
+            ]),
         };
 
-        let config = setup_dsp_provider_configuration().await;
+        let asset_response = asset_api::create_asset(&provider, Some(asset_input)).await;
 
-        println!("Config: {:?}", config);
+        assert!(asset_response.is_ok());
 
-        let result = negotiation_provider_api::request_negotiation(&config, request_message).await;
+        sleep(std::time::Duration::from_secs(5)).await;
 
-        match result {
-            Ok(negotiation) => {
+        let offer_id = Uuid::new_v4().to_string();
 
-                println!("Negotiation Requested Successful: {:?}", negotiation.clone());
+        let offer = Offer {
+            context: Some(LD_NS.to_string()),
+            at_type: Some("Offer".to_string()),
+            at_id: offer_id.clone(),
+            assigner: PROVIDER_ID.to_string(),
+            target: asset_id.clone(),
+        };
 
-                let consumer_pid = negotiation.clone().consumer_pid;
-                let provider_pid = negotiation.clone().provider_pid;
+        let policy: serde_json::Value = serde_json::json!({
+            "@context": offer.context,
+            "@type": offer.at_type,
+            "@id": offer.at_id,
+            "assigner": offer.assigner,
+            "target": offer.target,
+        });
 
-                let accept_message = ContractNegotiationEventMessage {
-                    context: std::collections::HashMap::from([("dspace".to_string(), serde_json::Value::String("https://w3id.org/dspace/v0.8/".to_string()))]),
-                    dsp_type: "dspace:ContractNegotiationEventMessage".to_string(),
-                    provider_pid: provider_pid.clone(),
-                    consumer_pid: consumer_pid.clone(),
-                    event_type: EventType::ACCEPTED,
+        let offer_description = ContractOfferDescription {
+            at_type: Some("OfferDescription".to_string()),
+            asset_id: Some(asset_id.clone()),
+            offer_id: Some(offer_id),
+            policy: Some(policy),
+        };
+
+        let contract_request = edc_api::ContractRequest {
+            context: std::collections::HashMap::from([("@vocab".to_string(), serde_json::Value::String("https://w3id.org/edc/v0.0.1/ns/".to_string()))]),
+            at_type: Some("ContractRequest".to_string()),
+            callback_addresses: None,
+            connector_address: None,
+            counter_party_address: "http://localhost:3000".to_string(),
+            offer: Some(offer_description),
+            policy: Some(offer.clone()),
+            protocol: DATASPACE_PROTOCOL.to_string(),
+            provider_id: None,
+        };
+
+        let contract_negotiation_response = contract_negotiation_api::initiate_contract_negotiation(&consumer, Some(contract_request)).await;
+
+        match contract_negotiation_response {
+            Ok(response) => {
+                assert!(response.at_id.is_some());
+                assert!(response.created_at.is_some());
+
+                println!("Negotiation: {:#?}", response);
+
+                sleep(std::time::Duration::from_secs(5)).await;
+
+                let cn_pid = response.at_id.clone().unwrap();
+                
+                let termination_request = edc_api::TerminateNegotiationSchema {
+                    context: std::collections::HashMap::from([("@vocab".to_string(), serde_json::Value::String("https://w3id.org/edc/v0.0.1/ns/".to_string()))]),
+                    at_id: cn_pid.clone(),
+                    at_type: None,
+                    reason: Some("Terminating for testing purposes".to_string()),
                 };
 
-                let accepted = negotiation_provider_api::accept_offer(&config, accept_message, provider_pid.clone().as_str()).await;
+                let terminate_response = contract_negotiation_api::terminate_negotiation(&consumer, cn_pid.clone().as_str(), Some(termination_request)).await;
 
-                println!("Negotiation Accept?: {:?}", accepted);
+                println!("Termination: {:#?}", terminate_response);
+                assert!(terminate_response.is_ok());
             }
             Err(e) => {
-                println!("Negotiation Requested Error: {:?}", e);
+                panic!("Error: {:#?}", e);
             }
         }
     }
