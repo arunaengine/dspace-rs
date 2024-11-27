468533b945406b18c5598f0519301522b2611aba
Maria Hansen <maria.hansen@computational.bio.uni-giessen.de>
2024-11-27 13:29:19 +0100

    feat: add sharedStates to method headers for now

--- crates/aruna_connector/src/api/transfer_process_provider.rs
+++ crates/aruna_connector/src/api/transfer_process_provider.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use std::sync::{Arc};
+use axum::extract::State;
 use tokio::sync::Mutex;
 use axum::response::IntoResponse;
 use dsp_api::transfer_process::TransferProcess;
@@ -7,26 +8,26 @@ use odrl::functions::state_machine::{ProviderState, ProviderStateMachine};
 
 type SharedState = Arc<Mutex<HashMap<String, (TransferProcess, ProviderStateMachine<ProviderState>)>>>;
 
-pub async fn get_transfer_process() -> impl IntoResponse {
+pub async fn get_transfer_process(State(state): State<SharedState>,) -> impl IntoResponse {
     unimplemented!()
 }
 
-pub async fn request_transfer_processes() -> impl IntoResponse {
+pub async fn request_transfer_processes(State(state): State<SharedState>,) -> impl IntoResponse {
     unimplemented!()
 }
 
-pub async fn start_transfer_process() -> impl IntoResponse {
+pub async fn start_transfer_process(State(state): State<SharedState>,) -> impl IntoResponse {
     unimplemented!()
 }
 
-pub async fn complete_transfer_process() -> impl IntoResponse {
+pub async fn complete_transfer_process(State(state): State<SharedState>,) -> impl IntoResponse {
     unimplemented!()
 }
 
-pub async fn terminate_transfer_process() -> impl IntoResponse {
+pub async fn terminate_transfer_process(State(state): State<SharedState>,) -> impl IntoResponse {
     unimplemented!()
 }
 
-pub async fn suspend_transfer_process() -> impl IntoResponse {
+pub async fn suspend_transfer_process(State(state): State<SharedState>,) -> impl IntoResponse {
     unimplemented!()
 }
