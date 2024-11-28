ef133f8f0e76b73b8d92eaad9a437ede8f21cbdb
Maria Hansen <maria.hansen@computational.bio.uni-giessen.de>
2024-11-28 10:47:46 +0100

    feat: implement request transfer process of the dsp api (provider side endpoint)

--- crates/aruna_connector/src/api/transfer_process_provider.rs
+++ crates/aruna_connector/src/api/transfer_process_provider.rs
@@ -1,13 +1,20 @@
 use std::collections::HashMap;
 use std::sync::{Arc};
 use axum::extract::{Path, State};
-use axum::http::StatusCode;
+use axum::http::{HeaderMap, StatusCode};
 use axum::Json;
 use tokio::sync::Mutex;
 use axum::response::IntoResponse;
+use serde_json::Value;
 use tracing::{debug, error, info};
+use uuid::Uuid;
+use dsp_api::transfer_process::transfer_process::{EDCTransferState, TransferStateType};
 use dsp_api::transfer_process::TransferProcess;
+use edc_api::transfer_state::TransferProcessState;
 use odrl::functions::state_machine::{ProviderState, ProviderStateMachine};
+
+// TODO: Add State Handling
+// TODO: Implement TP-specific State Machine
 
 type SharedState = Arc<Mutex<HashMap<String, (TransferProcess, ProviderStateMachine<ProviderState>)>>>;
 
@@ -29,8 +36,53 @@ pub async fn get_transfer_process(State(state): State<SharedState>, Path(pid): P
     }
 }
 
-pub async fn request_transfer_processes(State(state): State<SharedState>,) -> impl IntoResponse {
-    unimplemented!()
+pub async fn request_transfer_processes(headers: HeaderMap, State(state): State<SharedState>, Json(request_message): Json<Value>,) -> impl IntoResponse {
+    info!("[DSP] Request Transfer Process called");
+    debug!("[DSP] Request Body: {:#?}", request_message.clone());
+
+    let mut state = state.lock().await;
+
+    let host = headers["host"].to_str().unwrap();
+
+    if request_message["dspace:callbackAddress"].is_null() {
+        let err_msg = "The callbackAddress is required and should be a URL indicating where messages to the Consumer should be sent in asynchronous settings.".to_string();
+        error!("{}", err_msg);
+        return (StatusCode::UNPROCESSABLE_ENTITY, Json(err_msg)).into_response()
+    }
+
+    let mut fsm = ProviderStateMachine::new(
+        host.clone(),
+        request_message["dspace:callbackAddress"].as_str().unwrap(),
+    );
+
+    debug!("[DSP] Initialized new Transfer Process State Machine; State: {}", fsm.state);
+
+    let provider_pid = Uuid::new_v4().to_string();
+
+    let default_context = HashMap::from([
+        ("@vocab".to_string(), Value::String("https://w3id.org/edc/v0.0.1/ns/".to_string())),
+        ("edc".to_string(), Value::String("https://w3id.org/edc/v0.0.1/ns/".to_string())),
+        ("dcat".to_string(), Value::String("http://www.w3.org/ns/dcat#".to_string())),
+        ("dct".to_string(), Value::String("http://purl.org/dc/terms/".to_string())),
+        ("odrl".to_string(), Value::String("http://www.w3.org/ns/odrl/2/".to_string())),
+        ("dspace".to_string(), Value::String("https://w3id.org/dspace/v0.8/".to_string()))
+    ]);
+
+    let transfer_process = TransferProcess {
+        context: default_context,
+        dsp_type: "dspace:TransferProcess".to_string(),
+        provider_pid: provider_pid.clone(),
+        consumer_pid: request_message["dspace:consumerPid"].as_str().unwrap().to_string(),
+        state: TransferStateType::EDCTransferState(EDCTransferState::Initial),
+    };
+
+    debug!("[DSP] Contract Negotiation Provider PID: {:#?}", provider_pid.clone());
+    println!("[DSP] Transfer Process FSM: {:#?}", fsm.clone()); // only for demonstration purposes, remove later
+
+    state.insert(provider_pid.clone(), (transfer_process.clone(), fsm.clone()));
+
+    (StatusCode::CREATED, Json(transfer_process.clone())).into_response()
+
 }
 
 pub async fn start_transfer_process(State(state): State<SharedState>,) -> impl IntoResponse {
